c7ef882d2b5246d5084bd6bcde881eb0a5dd762f
Ixzaa <Ruth.Ixzayara.Bermudez.Cuba2809@gmail.com>
2023-09-23 22:28:54 -0600

    Imprimir el menu por consola

---
+++ Proyecto.Programacion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int opcion = 0;
+        do
+        {
+            Console.Clear(); // Limpiar la pantalla
+            Console.WriteLine("Bienvenido al menú de operaciones:");
+            Console.WriteLine("1. Pares e Impares");
+            Console.WriteLine("2. Tabla de multiplicar");
+            Console.WriteLine("3. Factorial de un número");
+            Console.WriteLine("4. Números Primos");
+            Console.WriteLine("5. Sucesión de Fibonacci");
+            Console.WriteLine("6. Salir");
+            Console.Write("Ingrese una opción del 1 al 6: ");
+
+        }
+         while (opcion != 6); // Continuar hasta que se seleccione la opción 6 (Salir)
+        
+    }
+}
